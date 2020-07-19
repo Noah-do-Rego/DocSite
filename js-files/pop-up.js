17a44d9515755cab64bdeafc5ba89180dd970b89
Noah-do-Rego <68390411+Noah-do-Rego@users.noreply.github.com>
2020-07-18 20:05:23 -0400

    Update pop-up.js

--- js-files/pop-up.js
+++ js-files/pop-up.js
@@ -1,7 +1,14 @@
-<script>
-// When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
-</script>
